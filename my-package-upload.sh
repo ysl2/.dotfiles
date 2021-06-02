bf52c169ec26a934600dc1fb935d98ad8088b6fe
ysl2 <songli.yu@outlook.com>
2021-06-02 14:51:13 +0800

    2021-06-02 14:51:13

--- my-package-upload.sh
+++ my-package-upload.sh
@@ -6,15 +6,8 @@ mkdir -p .MY-PACKAGE-LIST
 mkdir -p .MY-PACKAGE-LIST/${DE_WM}
 cd .MY-PACKAGE-LIST/${DE_WM}
 
-if [[ ! -f "my-pacman-list.txt" ]]; then
-	touch my-pacman-list.txt
-	touch my-yay-list.txt
-	touch my-pip-list.txt
-    touch my-npm-list.txt
-fi
-
-sudo pacman -Qeq > my-pacman-list.txt
-yay -Qeq > my-yay-list.txt
-pip list | awk '{print $1}' > my-pip-list.txt
-npm list -g --depth=0 > my-npm-list.txt
-
+sudo pacman -Qeq >my-pacman-list.txt
+yay -Qeq >my-yay-list.txt
+pip list | awk '{print $1}' >my-pip-list.txt
+npm list -g --depth=0 >my-npm-list.txt
+docker images >my-docker-list.txt
