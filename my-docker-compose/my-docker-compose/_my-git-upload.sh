c214a5e2cbc07dc7dbc3cdd1b76a8a87f669648f
ysl2 <songli.yu@outlook.com>
2021-06-06 23:44:53 +0800

    2021-06-06 23:44:53

--- my-docker-compose/my-docker-compose/_my-git-upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# upload my dotfiles
-# git submodule foreach 'git add . && git commit -m "$(date +"%Y-%m-%d %H:%M:%S")" && git push origin master'
-git add . && git commit -m "$(date +"%Y-%m-%d %H:%M:%S")" && git push origin master
