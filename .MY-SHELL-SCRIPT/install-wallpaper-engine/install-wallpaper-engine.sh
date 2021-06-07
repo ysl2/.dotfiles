ade6a63f074a8c95665a169c8275bd51b05e7ec9
ysl2 <songli.yu@outlook.com>
2021-06-07 22:11:28 +0800

    2021-06-07 22:11:28

--- .MY-SHELL-SCRIPT/install-wallpaper-engine/install-wallpaper-engine.sh
+++ .MY-SHELL-SCRIPT/install-wallpaper-engine/install-wallpaper-engine.sh
@@ -1,17 +1,18 @@
 #/usr/bin/bash
 
-# THIS=$(pwd)
-# cd ~/.config
-# git clone https://hub.fastgit.org/catsout/wallpaper-engine-kde-plugin.git
-# cd wallpaper-engine-kde-plugin
-# mkdir -p build && cd build
-# cmake -DCMAKE_BUILD_TYPE=Release ..
-# make
-# sudo make install
-# cd ../..
-# plasmapkg2 -i wallpaper-engine-kde-plugin/plugin
-# killall plasmashell && kstart5 plasmashell
-# cd "${THIS}"
+THIS=$(pwd)
+cd ~/.config
+git clone https://hub.fastgit.org/catsout/wallpaper-engine-kde-plugin.git
+cd wallpaper-engine-kde-plugin
+mkdir -p build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+sudo make install
+cd ../..
+plasmapkg2 -i wallpaper-engine-kde-plugin/plugin
+killall plasmashell && kstart5 plasmashell
+cd "${THIS}"
+
 clear
 echo '''YuSoLi: Almost finished. You need to do last two things:
 1. Open KDE wallpaper settings and choose the correct Steam folder. e.g: `~/.local/share/Steam`
