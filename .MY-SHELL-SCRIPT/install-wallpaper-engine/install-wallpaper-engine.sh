e60111e1863c6743202be3851ac37e38c91d69a9
ysl2 <songli.yu@outlook.com>
2021-06-07 21:06:27 +0800

    2021-06-07 21:06:27

---
+++ .MY-SHELL-SCRIPT/install-wallpaper-engine/install-wallpaper-engine.sh
@@ -0,0 +1,29 @@
+#/usr/bin/bash
+
+THIS=$(pwd)
+cd ~/.config
+git clone http://hub.fastgit.org/catsout/wallpaper-engine-kde-plugin.git
+cd wallpaper-engine-kde-plugin
+mkdir -p build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+sudo make install
+cd ..
+plasmapkg2 -i wallpaper-engine-kde-plugin/plugin
+killall plasmashell && kstart5 plasmashell
+cd "${THIS}"
+
+# If any error occurs, follow the steps below to rollback.
+#
+# 1. print `ctrl alt T` to open terminal, then input these commands:
+#
+#     ```bash
+#     nvim ~/.config/plasma-org.kde.plasma.desktop-appletsrc
+#     ```
+# 2. Find `WallpaperFilePath`, and delete the whole line.
+#
+# 3. Reboot KDE:
+#
+#     ```bash
+#     killall plasmashell && kstart5 plasmashell
+#     ```
