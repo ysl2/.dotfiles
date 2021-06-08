c1f5a72a97806cb65a1a72027cd2111977e1bd8c
ysl2 <songli.yu@outlook.com>
2021-06-08 09:15:20 +0800

    2021-06-08 09:15:20

--- zsh-oh-my-zsh/.myzsh/script/00-tmux.zsh
+++ zsh-oh-my-zsh/.myzsh/script/00-tmux.zsh
@@ -1,20 +1,12 @@
 tmux_init () {
-    if [[ ! -d ~/.tmux || ! -f "${MYZSH}"/.lock/tmux.lock || ! -h ~/.tmux.conf ]]; then
-        cd
-        # Add tmux framework
-        if [[ ! -d ~/.tmux ]]; then
-            git clone https://hub.fastgit.org/gpakosz/.tmux.git ~/.tmux
-            # git clone https://hub.fastgit.org/Johnny4Fun/.tmux.git ~/.tmux
-        fi
-        touch "${MYZSH}"/.lock/tmux.lock
-        if [[ $(yay -Qeq | grep stow) == 1 ]]; then
-            yay -S stow
-        fi
-        ln -s -f .tmux/.tmux.conf ~/.tmux.conf
-        cd ~/.dotfiles/
-        stow -R tmux
-        tmux source-file ~/.tmux.conf
+    cd
+    # Add tmux framework
+    if [[ ! -d ~/.tmux ]]; then
+        git clone https://hub.fastgit.org/gpakosz/.tmux.git ~/.tmux
     fi
+    touch "${MYZSH}"/.lock/tmux.lock
+    ln -s -f .tmux/.tmux.conf ~/.tmux.conf
+    tmux source-file ~/.tmux.conf
 }
 
 # init tmux
