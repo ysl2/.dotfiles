8dda5656d4965072ca0606eb622435f51d7d859b
ysl2 <songli.yu@outlook.com>
2021-06-08 09:18:19 +0800

    2021-06-08 09:18:19

---
+++ .MY-SHELL-SCRIPT/install-x11vnc/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+su -c "pacman -S x11vnc"
+mkdir -p ~/.local/share/systemd/user/
+cp x11vnc_user.service ~/.local/share/systemd/user/x11vnc_user.service
+systemctl --user daemon-reload
+systemctl enable --user x11vnc_user && systemctl start --user x11vnc_user
+chmod +x deactivate_compositor.sh
+sh ./deactivate_compositor.sh
