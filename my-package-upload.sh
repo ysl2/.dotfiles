a3ab1d1645001a7117d02232b96c7d4ef4453aa9
ubuntu <ubuntu@localhost.localdomain>
2021-06-09 10:06:04 +0800

    2021-06-09 10:06:04

--- my-package-upload.sh
+++ my-package-upload.sh
@@ -8,6 +8,6 @@ cd .MY-PACKAGE-LIST/${DE_WM}
 
 sudo pacman -Qeq >my-pacman-list.txt
 yay -Qeq >my-yay-list.txt
-pip list | awk '{print $1}' >my-pip-list.txt
+pip3 list | awk '{print $1}' >my-pip-list.txt
 npm list -g --depth=0 >my-npm-list.txt
 docker images | awk '{print $1}' >my-docker-list.txt
