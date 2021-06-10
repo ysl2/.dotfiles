1036ab0e5035177bbb6218e335232e3d11e0a88b
ubuntu <ubuntu@localhost.localdomain>
2021-06-10 13:40:30 +0800

    2021-06-10 13:40:30

--- my-docker/my-docker/my-docker-compose/my-dnmp/install.sh
+++ my-docker/my-docker/my-docker-compose/my-dnmp/install.sh
@@ -2,11 +2,9 @@
 THIS=$(pwd)
 DATA=~/my-docker/my-docker-volumes/
 
-mkdir -p $DATA
 cd $DATA
-git clone https://hub.fastgit.org/yeszao/dnmp.git my-dnmp
+git clone https://hub.fastgit.org/yeszao/dnmp.git
+\mv dnmp/* my-dnmp/
+rm -rf dnmp
 cd my-dnmp
-for item in $(ls -A ../../my-docker-compose/my-dnmp/ | grep -v install.sh); do
-  ln -s ../../my-docker-compose/my-dnmp/"$item" .
-done
 docker-compose up -d
