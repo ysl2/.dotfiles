b6a1c8188d6003b495bd3d8465c731ddd9b797b4
ubuntu <ubuntu@localhost.localdomain>
2021-06-10 15:59:26 +0800

    2021-06-10 15:59:26

--- my-docker/my-docker/my-docker-compose/my-dnmp/install.sh
+++ my-docker/my-docker/my-docker-compose/my-dnmp/install.sh
@@ -5,6 +5,7 @@ DATA=~/my-docker/my-docker-volumes/
 cd $DATA
 git clone https://hub.fastgit.org/yeszao/dnmp.git
 mv -n dnmp/* my-dnmp/
+mv -n dnmp/.* my-dnmp/
 rm -rf dnmp
 cd my-dnmp
 docker-compose up -d
