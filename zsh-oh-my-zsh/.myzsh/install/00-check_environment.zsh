c00c9b0c6ba908028adb0af2e4c0220c109bf138
ysl2 <songli.yu@outlook.com>
2021-06-12 07:18:00 +0800

    2021-06-12 07:18:00

--- zsh-oh-my-zsh/.myzsh/install/00-check_environment.zsh
+++ zsh-oh-my-zsh/.myzsh/install/00-check_environment.zsh
@@ -4,13 +4,13 @@ if [[ ! -e ~/.ssh ]]; then
   exit 1
 fi
 
-# 0: stdin
-# 1: stdout
-# 2: stderr
-# `&>/dev/null`的意思是：`1>/dev/null 2>&1`
-# 即先把stdout重定向到/dev/null, 再把stderr重定向到stdout的位置
-if ! cargo &>/dev/null; then
-  echo 'YuSoLi: cargo is not installed.'
-  exit 1
-fi
+# # 0: stdin
+# # 1: stdout
+# # 2: stderr
+# # `&>/dev/null`的意思是：`1>/dev/null 2>&1`
+# # 即先把stdout重定向到/dev/null, 再把stderr重定向到stdout的位置
+# if ! cargo &>/dev/null; then
+#   echo 'YuSoLi: cargo is not installed.'
+#   exit 1
+# fi
 
