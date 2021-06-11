d657640bf6a8805b5e96ed492368f95791aaab5a
ysl2 <songli.yu@outlook.com>
2021-06-11 10:28:50 +0800

    2021-06-11 10:28:50

--- zsh-oh-my-zsh/.myzsh/install/01-install_brew.zsh
+++ zsh-oh-my-zsh/.myzsh/install/01-install_brew.zsh
@@ -1,4 +1,4 @@
 if [[ ! -e "${MYZSH}"/.lock/brew.lock ]]; then
-  sh "${MYZSH}"/install/assets/install-brew.sh
+  bash "${MYZSH}"/install/assets/install-brew.sh
   touch "${MYZSH}"/.lock/brew.lock
 fi
