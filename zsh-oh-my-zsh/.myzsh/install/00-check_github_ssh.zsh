a5053c745da7562c6ff1b3ef343e86e31a90f2fc
ysl2 <songli.yu@outlook.com>
2021-06-11 13:42:30 +0800

    2021-06-11 13:42:30

--- zsh-oh-my-zsh/.myzsh/install/00-check_github_ssh.zsh
+++
@@ -1,6 +0,0 @@
-# 通过普通用户的ssh链接下载。因此如果没有设置ssh,就报错退出。
-if [[ ! -e ~/.ssh ]]; then
-  echo "YSL: no github ssh key found." >&2
-  exit 1
-fi
-
--- zsh-oh-my-zsh/.myzsh/source/00-source_omz_plugins.zsh
+++ zsh-oh-my-zsh/.myzsh/source/00-source_omz_plugins.zsh
@@ -6,7 +6,7 @@ if [[ ! -e "${MYZSH}"/.lock/omz-echo.lock ]]; then
   # 每次解除锁之后，都会输出一次自己加载了哪些插件。然后再加上锁，下次就不会输出了。
   # 这个输出的作用是为了debug。
   echo ''
-  echo "YSL: Plugin list"
+  echo "YuSoLi: Plugin list"
   echo "================"
   for item in $plugins; do
     echo $item
