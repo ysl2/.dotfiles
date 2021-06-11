d10501e5c5f9b4beec526f854cdb0c9fad63a377
ysl2 <songli.yu@outlook.com>
2021-06-11 08:49:17 +0800

    2021-06-11 08:49:17

--- zsh-oh-my-zsh/.myzsh/aliases/aliases.zsh
+++ zsh-oh-my-zsh/.myzsh/aliases/aliases.zsh
@@ -1,5 +1,10 @@
+# ===============
+# === Aliases ===
+# ===============
+
+
 # ===
-# === Aliases
+# === goto alias
 # ===
 alias gh="cd"
 alias up="cd .."
@@ -11,8 +16,18 @@ alias gc='cd ~/.config/coc/'
 # 下面这个需要根据不同的电脑进行更改
 alias oo='cd ~/Documents/00-Github/; code ./00-my-notes; cd -'
 
-# 功能性别名
+
+# ===
+# === functional alias
+# ===
+alias ':q'='exit'
+# omzplugin
 alias ex="extract"
+
+
+# ===
+# === third-party software
+# ===
 alias cht="cht.sh"
 alias t='tmux'
 alias ta='tmux attach'
@@ -34,32 +49,48 @@ alias grep='rg'
 alias igrep='peco'
 alias top='htop'
 alias btop='bashtop'
-alias ctop='ctop'
 alias diff='delta'
 alias hack='btm'
 alias traceroute='mtr'
 alias df='pydf'
 alias ftp='lftp'
+# zoxide
 alias cd='z'
 alias cdi='zi'
+alias ctop='ctop'
 alias wireshark='tcpdump'
+alias ping='prettyping --nolegend'
+# pip install httpie
+alias https='http --default-scheme=https'
+alias wttr='curl zh.wttr.in\/WeiHai\?2Fq'
 
+
+# ===
+# === arch package manager
+# ===
 alias rls='exa --color=always'
 alias rless='less -r'
 alias rgrep='rg --color=always'
 alias pacman='sudo powerpill'
 alias rpacman='sudo powerpill --color=always'
 alias ryay='yay --color=always'
-
 # 拉取更新用的
 alias sss='sudo pacman -Syu --noconfirm'
 alias yyy='yay -Syu --noconfirm'
 alias goodbye='systemctl suspend'
 
+
+# ===
+# === tmux
+# ===
 # 刷新tmux的快捷键
 alias TS='tmux source-file ~/.tmux.conf'
 alias TL='rm ~/.tmux/.tmux.lock'
 
+
+# ===
+# === debug zsh & desktop mamager
+# ===
 # 重置zsh用的快捷键
 alias S='source ~/.zshrc'
 alias L='rm "${MYZSH}"/.lock/omz.lock'
