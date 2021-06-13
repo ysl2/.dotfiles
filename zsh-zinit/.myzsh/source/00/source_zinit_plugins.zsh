6798833851f92133ee9a740e4a031b942575fda1
ysl2 <songli.yu@outlook.com>
2021-06-13 13:53:03 +0800

    2021-06-13 13:53:03

--- zsh-zinit/.myzsh/source/00/source_zinit_plugins.zsh
+++ zsh-zinit/.myzsh/source/00/source_zinit_plugins.zsh
@@ -29,19 +29,20 @@ fi
 # === load first
 # ===
 # 让tmux先加载
-# zinit ice from"hub.fastgit.org" lucid atinit"
-# export TMUX_MOTD=false
-# "
-# zinit light zpm-zsh/tmux
-# tmux 配置字段参考这里：https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/tmux
-zinit ice lucid atinit"
-export ZSH_TMUX_AUTOSTART=true
-export ZSH_TMUX_AUTOSTART_ONCE=true
-export ZSH_TMUX_AUTOCONNECT=true
-export ZSH_TMUX_AUTOQUIT=true
-export ZSH_TMUX_FIXTERM=false
+zinit ice from"hub.fastgit.org" lucid atinit"
+export TMUX_MOTD=false
 "
-zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/plugins/tmux/tmux.plugin.zsh'
+zinit light zpm-zsh/tmux
+
+# # tmux 配置字段参考这里：https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/tmux
+# zinit ice lucid atinit"
+# export ZSH_TMUX_AUTOSTART=true
+# export ZSH_TMUX_AUTOSTART_ONCE=true
+# export ZSH_TMUX_AUTOCONNECT=true
+# export ZSH_TMUX_AUTOQUIT=true
+# export ZSH_TMUX_FIXTERM=false
+# "
+# zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/plugins/tmux/tmux.plugin.zsh'
 
 
 # ===
