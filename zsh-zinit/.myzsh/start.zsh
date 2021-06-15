460217202df31ecf577ba37f9cf36a904423dcb0
ysl2 <songli.yu@outlook.com>
2021-06-15 13:40:42 +0800

    2021-06-15 13:40:42

--- zsh-zinit/.myzsh/start.zsh
+++ zsh-zinit/.myzsh/start.zsh
@@ -2,7 +2,7 @@
 # === set environment
 # ===
 # for item in $(\ls "${MYZSH}"/install); do
-for item in "${MYZSH}"/env/*.zsh; do
+for item in "${MYZSH}"/env/**/*.zsh; do
   # source "${MYZSH}"/install/"${item}"
   source $item
 done
@@ -12,7 +12,7 @@ done
 # === install needed
 # ===
 # for item in $(\ls "${MYZSH}"/install); do
-for item in "${MYZSH}"/install/*.zsh; do
+for item in "${MYZSH}"/install/**/*.zsh; do
   # source "${MYZSH}"/install/"${item}"
   source $item
 done
@@ -21,7 +21,7 @@ done
 # === source environmental settings
 # ===
 # for item in $(\ls "${MYZSH}"/source); do
-for item in "${MYZSH}"/source/*/*.zsh; do
+for item in "${MYZSH}"/source/**/*.zsh; do
   # source "${MYZSH}"/source/"${item}"
   source $item
 done
@@ -31,7 +31,7 @@ done
 # === source others (e.g conda)
 # ===
 # for item in $(\ls "${MYZSH}"/func); do
-for item in "${MYZSH}"/func/*.zsh; do
+for item in "${MYZSH}"/func/**/*.zsh; do
   # source "${MYZSH}"/func/"${item}"
   source $item
 done
@@ -40,7 +40,7 @@ done
 # ===
 # === source aliases
 # ===
-for item in "${MYZSH}"/aliases/*.zsh; do
+for item in "${MYZSH}"/aliases/**/*.zsh; do
   # source "${MYZSH}"/aliases/"${item}"
   source $item
 done
