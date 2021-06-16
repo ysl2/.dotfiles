39fda02e79e52c95c41ac9b164de2aab00a92149
ysl2 <songli.yu@outlook.com>
2021-06-16 09:07:10 +0800

    2021-06-16 09:07:10

--- tmux/archive-oh-my-tmux/README.md
+++ tmux/archive-oh-my-tmux/README.md
@@ -1,5 +1,5 @@
 # Used for oh-my-tmux
 
-Beacuse of the boot lag, I temporarily stop to use oh-my-tmux.
+Beacuse of the boot lag, I temporarily stop using oh-my-tmux.
 
 The `.tmux.conf.local` is the main dotfile, the others are used for special environments.
---
+++ zsh-zinit/.myzsh/main/00-env/sys.zsh
@@ -0,0 +1,31 @@
+# ============
+# === PATH ===
+# ============
+
+# export PATH=$PATH:~/.cargo/bin
+# export PATH=$PATH:/usr/local/go/bin
+# export PATH=$PATH:~/go/bin
+
+# export FPATH=$FPATH:/usr/share/zsh/site-functions
+# export FPATH=$FPATH:/usr/share/zsh/functions/Calendar
+# export FPATH=$FPATH:/usr/share/zsh/functions/Chpwd
+# export FPATH=$FPATH:/usr/share/zsh/functions/Completion
+# export FPATH=$FPATH:/usr/share/zsh/functions/Completion/Base
+# export FPATH=$FPATH:/usr/share/zsh/functions/Completion/Linux
+# export FPATH=$FPATH:/usr/share/zsh/functions/Completion/Unix
+# export FPATH=$FPATH:/usr/share/zsh/functions/Completion/X
+# export FPATH=$FPATH:/usr/share/zsh/functions/Completion/Zsh
+# export FPATH=$FPATH:/usr/share/zsh/functions/Exceptions
+# export FPATH=$FPATH:/usr/share/zsh/functions/Math
+# export FPATH=$FPATH:/usr/share/zsh/functions/MIME
+# export FPATH=$FPATH:/usr/share/zsh/functions/Misc
+# export FPATH=$FPATH:/usr/share/zsh/functions/Newuser
+# export FPATH=$FPATH:/usr/share/zsh/functions/Prompts
+# export FPATH=$FPATH:/usr/share/zsh/functions/TCP
+# export FPATH=$FPATH:/usr/share/zsh/functions/VCS_Info
+# export FPATH=$FPATH:/usr/share/zsh/functions/VCS_Info/Backends
+# export FPATH=$FPATH:/usr/share/zsh/functions/Zftp
+# export FPATH=$FPATH:/usr/share/zsh/functions/Zle
+
+export FPATH=$FPATH:/usr/share/zsh/site-functions:/usr/share/zsh/functions/Calendar:/usr/share/zsh/functions/Chpwd:/usr/share/zsh/functions/Completion:/usr/share/zsh/functions/Completion/Base:/usr/share/zsh/functions/Completion/Linux:/usr/share/zsh/functions/Completion/Unix:/usr/share/zsh/functions/Completion/X:/usr/share/zsh/functions/Completion/Zsh:/usr/share/zsh/functions/Exceptions:/usr/share/zsh/functions/Math:/usr/share/zsh/functions/MIME:/usr/share/zsh/functions/Misc:/usr/share/zsh/functions/Newuser:/usr/share/zsh/functions/Prompts:/usr/share/zsh/functions/TCP:/usr/share/zsh/functions/VCS_Info:/usr/share/zsh/functions/VCS_Info/Backends:/usr/share/zsh/functions/Zftp:/usr/share/zsh/functions/Zle
+
