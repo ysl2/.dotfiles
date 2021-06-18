7e378dc8aa9090bf9441c2e0600d5313101ada86
ysl2 <songli.yu@outlook.com>
2021-06-18 18:24:05 +0800

    2021-06-18 18:24:05

--- zsh-zinit/.myzsh/main/00-env/env_self.zsh
+++ zsh-zinit/.myzsh/main/00-env/env_self.zsh
@@ -5,3 +5,9 @@ export EDITOR=nvim
 export DOTFILES_MAIN="${HOME}"/.dotfiles-main
 export DOTFILES="${HOME}"/.dotfiles
 # export THEME=spaceship
+
+
+# ===
+# === fzf
+# ===
+export FZF_DEFAULT_COMMAND='fd --hidden'
