46c5f991e808e52d69e333f74a041b8210b63d5e
ysl2 <songli.yu@outlook.com>
2021-06-20 08:49:35 +0800

    2021-06-20 08:49:35

--- zsh-zinit/.myzsh/main/00-env/env_self.zsh
+++ zsh-zinit/.myzsh/main/00-env/env_self.zsh
@@ -5,6 +5,10 @@ export EDITOR=nvim
 export DOTFILES_MAIN="${HOME}"/.dotfiles-main
 export DOTFILES="${HOME}"/.dotfiles
 # export THEME=spaceship
+if [ $TERM = 'alacritty' ]
+then
+  export TERM=xterm-256color
+fi
 
 
 # ===
--- zsh-zinit/.myzsh/main/02-source/02-source_omz_plugins.zsh
+++ zsh-zinit/.myzsh/main/02-source/02-source_omz_plugins.zsh
@@ -12,27 +12,55 @@ zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/lib/git.zsh'
 zinit ice wait lucid
 zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/lib/theme-and-appearance.zsh'
 
-# vi
+# ===
+# === vi
+# ===
 zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/plugins/vi-mode/vi-mode.plugin.zsh'
-# fzf
+
+
+# ===
+# === fzf
+# ===
 zinit ice wait lucid atinit"
 export DISABLE_FZF_KEY_BINDINGS=true
 "
 zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/plugins/fzf/fzf.plugin.zsh'
-# git
+
+# zinit ice wait lucid from"hub.fastgit.org"
+# zinit light Aloxaf/fzf-tab
+
+
+# ===
+# === git
+# ===
 zinit ice wait lucid atload$'unalias grv g 2>/dev/null'
 zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/plugins/git/git.plugin.zsh'
-# colored-man-pages
+
+
+# ===
+# === colored-man-pages
+# ===
 zinit ice wait'1' lucid
 zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/plugins/colored-man-pages/colored-man-pages.plugin.zsh'
-# autojump
+
+
+# ===
+# === autojump
+# ===
 zinit ice wait lucid
 zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/plugins/autojump/autojump.plugin.zsh'
-# command-not-found
+
+
+# ===
+# === command-not-found
+# ===
 zinit ice wait'1' lucid
 zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/plugins/command-not-found/command-not-found.plugin.zsh'
 
-# extract
+
+# ===
+# === extract
+# ===
 # zinit ice svn lucid wait='1'
 # zinit ice lucid wait='1'
 # zinit snippet 'https://cdn.jsdelivr.net/gh/ohmyzsh/ohmyzsh/plugins/extract'
