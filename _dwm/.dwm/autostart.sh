65a30cfe2160c6e11d3b75a1f00bcf09e8b9442b
ysl2 <songli.yu@outlook.com>
2021-06-22 14:46:40 +0800

    add nm-applet

--- _dwm/.dwm/autostart.sh
+++ _dwm/.dwm/autostart.sh
@@ -10,7 +10,7 @@ picom -b
 # /bin/bash ~/scripts/tap-to-click.sh &
 # /bin/bash ~/scripts/inverse-scroll.sh &
 # /bin/bash ~/scripts/setxmodmap-colemak.sh &
-# nm-applet &
+nm-applet &
 # xfce4-power-manager &
 #xfce4-volumed-pulse &
 # /bin/bash ~/scripts/run-mailsync.sh &
