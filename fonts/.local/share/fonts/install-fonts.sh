3a6477088770c21661fda3db8dce06ab769530c5
ysl2 <songli.yu@outlook.com>
2021-07-04 23:56:05 +0800

    2021-07-04 23:56:05

--- fonts/.local/share/fonts/install-fonts.sh
+++ fonts/.local/share/fonts/install-fonts.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-git clone https://hub.fastgit.org/laishulu/Sarasa-Mono-SC-Nerd.git
+git clone https://hub.fastgit.org/ysl2/Sarasa-Mono-SC-Nerd.git ~/.local/share/fonts/sarasa
+mv ~/.local/share/fonts/sarasa/sarasa* ~/.local/share/fonts/
+rm -rf ~/.local/share/fonts/sarasa
+
