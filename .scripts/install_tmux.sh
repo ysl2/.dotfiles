c5550cc68edee7e73ee4f2f467653274625002d8
ysl2 <www.songli.yu@gmail.com>
2023-04-07 17:00:26 +0800

    Update .scripts/install_tmux.sh

--- .scripts/install_tmux.sh
+++ .scripts/install_tmux.sh
@@ -1,56 +1,80 @@
 #!/bin/bash
 
 # Script for installing tmux on systems where you don't have root access.
-# tmux will be installed in $HOME/local/bin.
+# tmux will be installed in ${PREFIX}/bin.
 # It's assumed that wget and a C/C++ compiler are installed.
 
 # exit on error
 set -e
 
+PREFIX=$HOME/.Local
+TMUX_TMP=$HOME/tmux_tmp
+
+OPENSSL_VERSION=3.1.0
+LIBEVENT_VERSION=2.1.12-stable
+NCURSES_VERSION=6.4
 TMUX_VERSION=3.3a
 
 # create our directories
-mkdir -p $HOME/local $HOME/tmux_tmp
-cd $HOME/tmux_tmp
+mkdir -p ${PREFIX} ${TMUX_TMP}
+cd ${TMUX_TMP}
 
 # download source files for tmux, libevent, and ncurses
-wget -O tmux-${TMUX_VERSION}.tar.gz https://ghproxy.com/https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
-wget https://ghproxy.com/https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz
-wget https://ghproxy.com/https://github.com/mirror/ncurses/archive/refs/tags/v6.4.tar.gz && mv v6.4.tar.gz ncurses.tar.gz
+[[ ! -e openssl-${OPENSSL_VERSION}.tar.gz ]] && wget https://ghproxy.com/https://github.com/openssl/openssl/releases/download/openssl-3.1.0/openssl-${OPENSSL_VERSION}.tar.gz
+[[ ! -e libevent-${LIBEVENT_VERSION}.tar.gz ]] && wget https://ghproxy.com/https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}/libevent-${LIBEVENT_VERSION}.tar.gz
+[[ ! -e v${NCURSES_VERSION}.tar.gz ]] && wget https://ghproxy.com/https://github.com/mirror/ncurses/archive/refs/tags/v${NCURSES_VERSION}.tar.gz
+[[ ! -e tmux-${TMUX_VERSION}.tar.gz ]] && wget https://ghproxy.com/https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
 
 # extract files, configure, and compile
 
-############
-# libevent #
-############
-tar xvzf libevent-2.1.12-stable.tar.gz
-cd libevent-2.1.12-stable
-./configure --prefix=$HOME/local --disable-shared
+
+# ================
+# === libevent ===
+# ================
+
+# ===
+# === openssl
+# ===
+tar xvzf openssl-${OPENSSL_VERSION}.tar.gz
+cd openssl-${OPENSSL_VERSION}
+./Configure --prefix=${PREFIX}
 make
 make install
 cd ..
 
-############
-# ncurses  #
-############
-tar xvzf ncurses.tar.gz
-cd ncurses-6.4
-./configure --prefix=$HOME/local CPPFLAGS="-P"
+# ===
+# === libevent
+# ===
+tar xvzf libevent-${LIBEVENT_VERSION}.tar.gz
+cd libevent-${LIBEVENT_VERSION}
+./configure PKG_CONFIG_PATH=${PREFIX}/lib64/pkgconfig --prefix=${PREFIX} --disable-shared
 make
 make install
 cd ..
 
-############
-# tmux     #
-############
+
+# ===============
+# === ncurses ===
+# ===============
+tar xvzf v${NCURSES_VERSION}.tar.gz
+cd ncurses-${NCURSES_VERSION}
+./configure --prefix=${PREFIX} CPPFLAGS="-P"
+make
+make install
+cd ..
+
+
+# ============
+# === tmux ===
+# ============
 tar xvzf tmux-${TMUX_VERSION}.tar.gz
 cd tmux-${TMUX_VERSION}
-./configure CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
-CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
-cp tmux $HOME/local/bin
+./configure CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/ncurses" LDFLAGS="-L${PREFIX}/lib -L${PREFIX}/include/ncurses -L${PREFIX}/include"
+CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/ncurses" LDFLAGS="-static -L${PREFIX}/include -L${PREFIX}/include/ncurses -L${PREFIX}/lib" make
+cp tmux ${PREFIX}/bin
 cd ..
 
 # cleanup
-rm -rf $HOME/tmux_tmp
+rm -rf ${TMUX_TMP}
 
-echo "$HOME/local/bin/tmux is now available. You can optionally add $HOME/local/bin to your PATH."
+echo "Tmux is now available. You can add tmux location to your PATH."
