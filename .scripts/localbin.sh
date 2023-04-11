5458841e016e26aa3c517f1ccfc8f9274fb11d22
ysl2 <www.songli.yu@gmail.com>
2023-04-11 12:01:12 +0800

    Fix bug in localbin.sh

--- .scripts/localbin.sh
+++ .scripts/localbin.sh
@@ -208,6 +208,7 @@ function termscp () {
     TERMSCP_VERSION=v0.11.1
     [[ ! -e termscp-${TERMSCP_VERSION}-x86_64-unknown-linux-gnu.tar.gz ]] && wget https://github.com/veeso/termscp/releases/download/${TERMSCP_VERSION}/termscp-${TERMSCP_VERSION}-x86_64-unknown-linux-gnu.tar.gz
     tar xzvf termscp-${TERMSCP_VERSION}-x86_64-unknown-linux-gnu.tar.gz
+    mkdir -p ${PREFIX}/bin
     mv termscp ${PREFIX}/bin
 }
 
