c7b7d6182af02eb3267c494e28c73b7ce1e74dfd
ysl2 <www.songli.yu@gmail.com>
2023-04-15 02:18:40 +0800

    Change jupyter notebook to jupyter lab

--- .jupyter/init.sh
+++ .jupyter/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # python3 -c "from notebook.auth import passwd; print(passwd())"
-jupyter notebook \
+# jupyter notebook \
+jupyter lab \
     --ip=0.0.0.0 \
     --port=8888 \
     --notebook-dir=~/Public/jupyter \
