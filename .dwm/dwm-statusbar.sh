1217383a46bc49e00dbfcd452a0489dc6316d14d
ysl2 <www.songli.yu@gmail.com>
2023-11-03 22:22:12 +0800

    Update dwm statusbar, remove whitespaces.

--- .dwm/dwm-statusbar.sh
+++ .dwm/dwm-statusbar.sh
@@ -4,36 +4,42 @@ get_cpu_usage() {
     cpu_percentage=$(top -bn1 | grep "%Cpu(s)" | awk '{print $2}' | cut -d "." -f 1)
     cpu_icon=""
     printf -v cpu_percentage "%2s" "$cpu_percentage"
-    echo "$cpu_icon $cpu_percentage%"
+    echo "$cpu_icon $cpu_percentage% "
 }
 
 get_memory_usage() {
     memory_info=$(free -h | awk '/^Mem/ {print $3 "/" $2}')
     memory_icon=""
-    echo "$memory_icon $memory_info"
+    echo "$memory_icon $memory_info "
 }
 
 get_root_disk_usage() {
     disk_usage=$(df -h / | awk 'END{print $3 "/" $2}')
     disk_icon=""
-    echo "$disk_icon $disk_usage"
+    echo "$disk_icon $disk_usage "
 }
 
 get_volume() {
     volume=$(amixer get Master | grep -oE '[0-9]{1,3}%' | head -1)
     volume_icon=""
     printf -v volume "%3s" "$volume"
-    echo "$volume_icon $volume"
+    echo "$volume_icon $volume "
 }
 
 get_datetime() {
     datetime=$(date +"%Y-%m-%d %a %H:%M:%S")
     clock_icon=""
-    echo "$clock_icon $datetime"
+    echo "$clock_icon $datetime "
 }
 
 get_battery() {
-    status=$(cat /sys/class/power_supply/BAT0/status)
+    status_file=/sys/class/power_supply/BAT0/status
+    capacity_file=/sys/class/power_supply/BAT0/capacity
+    if [ ! -e "$status_file" ] || [ ! -e "$capacity_file" ]; then
+        echo ''
+        return
+    fi
+    status=$(cat "$status_file")
 
     if [ "$status" = "Charging" ]; then
         battery_icon=""
@@ -42,9 +48,13 @@ get_battery() {
     else
         battery_icon=""
     fi
-    capacity=$(cat /sys/class/power_supply/BAT0/capacity)
-    printf -v capacity "%2s" "$capacity"
-    echo "$battery_icon $capacity%"
+    capacity=$(cat "$capacity_file")
+    if [ -z "$capacity" ]; then
+        echo ''
+    else
+        printf -v capacity "%3s" "$capacity"
+        echo "$battery_icon $capacity% "
+    fi
 }
 
 while true; do
@@ -55,7 +65,7 @@ while true; do
     battery_info=$(get_battery)
     datetime_info=$(get_datetime)
 
-    status_text="$cpu_info $memory_info $disk_info $volume_info $battery_info $datetime_info"
+    status_text="${cpu_info}${memory_info}${disk_info}${volume_info}${battery_info}${datetime_info}"
 
     xsetroot -name "$status_text"
 
