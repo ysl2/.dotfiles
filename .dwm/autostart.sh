e9843c5c824fc852f71d0bcced2680f8c52a97c5
ysl2 <www.songli.yu@gmail.com>
2023-11-11 11:30:24 +0800

    single instance slstatus

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -4,14 +4,13 @@ picom -b
 ~/.dwm/dwm-natural-scrolling.sh
 
 single_instance() {
-    result=$(ps aux | grep -v grep | grep  "$1")
-    if [ ! "$result" == '' ]; then
+    result=$(ps aux | grep -v grep | grep "$1")
+    if [ "$result" != '' ]; then
         killall "$1"
     fi
-    ~/.dwm/"$1" &
+    "$1" &
 }
 
-#single_instance dwm-statusbar.sh
-slstatus &
+single_instance slstatus
 
 [ -f ~/.dwm/autostart.localhost.sh ] && ~/.dwm/autostart.localhost.sh
