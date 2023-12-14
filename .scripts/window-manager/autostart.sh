9354a282340582aa5b4bcc93340dc4bf8a328f15
ysl2 <www.songli.yu@gmail.com>
2023-12-14 15:12:16 +0800

    Update autoload scripts.

--- .scripts/window-manager/autostart.sh
+++ .scripts/window-manager/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-~/.scripts/window-manager/natural-scrolling.sh
-~/.scripts/window-manager/keyboard.sh
+~/.scripts/window-manager/natural_scrolling.sh
+~/.scripts/window-manager/xset.sh
 picom -b
 
 [ -f ~/.scripts/window-manager/autostart.localhost.sh ] && ~/.scripts/window-manager/autostart.localhost.sh
--- .scripts/window-manager/keyboard.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/bin/setxkbmap -option caps:swapescape
-/usr/bin/xset r rate 250 30
