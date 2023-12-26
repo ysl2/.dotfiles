d76a38e8dd49625c2e141da29b1746dd9c16d02f
ysl2 <www.songli.yu@gmail.com>
2023-12-26 18:30:11 +0800

    Fix bug in ~/.scripts/window-manager/autostart.sh

--- .scripts/window-manager/autostart.sh
+++ .scripts/window-manager/autostart.sh
@@ -9,30 +9,33 @@ single_instance=~/.scripts/window-manager/utils/single_instance.sh
 # ================
 # === Touchpad ===
 # ================
-if [ "$XDG_SESSION_TYPE" = x11 ]; then
-    # Get id of touchpad
-    id="$(xinput list | grep 'Touchpad'  | cut -d'=' -f2 | awk '{print $1}')"
-    # Get id of the field corresponding to natural scrolling
-    id_natural_scrolling="$(xinput list-props "$id" | \
-                          grep 'Natural Scrolling Enabled (' \
-                          | cut -d'(' -f2 | cut -d')' -f1)"
-    # Get id of the field corresponding to tapping
-    id_tapping_enabled="$(xinput list-props "$id" | \
-                          grep 'Tapping Enabled (' \
-                          | cut -d'(' -f2 | cut -d')' -f1)"
-    # Set the property to true
-    xinput --set-prop "$id" "$id_natural_scrolling" 1
-    xinput --set-prop "$id" "$id_tapping_enabled" 1
-fi
+case "$XDG_SESSION_TYPE" in
+    x11 | tty)
+        # Get id of touchpad
+        id="$(xinput list | grep 'Touchpad'  | cut -d'=' -f2 | awk '{print $1}')"
+        # Get id of the field corresponding to natural scrolling
+        id_natural_scrolling="$(xinput list-props "$id" | \
+                              grep 'Natural Scrolling Enabled (' \
+                              | cut -d'(' -f2 | cut -d')' -f1)"
+        # Get id of the field corresponding to tapping
+        id_tapping_enabled="$(xinput list-props "$id" | \
+                              grep 'Tapping Enabled (' \
+                              | cut -d'(' -f2 | cut -d')' -f1)"
+        # Set the property to true
+        xinput --set-prop "$id" "$id_natural_scrolling" 1
+        xinput --set-prop "$id" "$id_tapping_enabled" 1
+        ;;
+esac
 
 
 # ================
 # === Keyboard ===
 # ================
-if [ "$XDG_SESSION_TYPE" = x11 ]; then
+case "$XDG_SESSION_TYPE" in
+    x11 | tty)
     # Switch capslock and escape
     setxkbmap -option caps:swapescape
-fi
+esac
 # Acclerate keyboard
 xset r rate 250 30
 # Disable beep sound
