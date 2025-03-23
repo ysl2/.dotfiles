c108da6023f5b14d6226748958298080fd8816b1
ysl2 <www.songli.yu@gmail.com>
2025-03-23 15:29:17 +0800

    Update ~/.config/yazi
    Add drag & drop for yazi
    
    Fix

---
+++ .config/yazi/init.lua
@@ -0,0 +1,5 @@
+-- NOTE: Use `ya pack -i` to restore all plugins.
+
+-- NOTE: `ya pack -a dedukun/relative-motions`
+-- Ref: https://github.com/dedukun/relative-motions.yazi?tab=readme-ov-file#configuration
+require("relative-motions"):setup({ show_numbers="relative_absolute", show_motion = true, enter_mode ="first" })
